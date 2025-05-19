d8e140ab0b6df1b5d7fb62c134f76331594e232c
Ponche <142775540+Ponche03@users.noreply.github.com>
2025-05-19 11:37:56 -0600

    firebaseAdmin fix

--- Back-End/utils/firebaseAdmin.js
+++ Back-End/utils/firebaseAdmin.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./studify-5e81f-firebase-adminsdk-fbsvc-e821d8811e.json");
+var serviceAccount = require("../studify-5e81f-firebase-adminsdk-fbsvc-e821d8811e.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
